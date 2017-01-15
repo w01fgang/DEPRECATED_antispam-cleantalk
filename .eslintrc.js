e6c37d658e2cfc992b67d6ee394efdaa9f8503c7
w01fgang <sumin@unix-center.ru>
2017-01-15 20:26:21 +0100

    add jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
     "extends": "airbnb",
     "installedESLint": true,
+    "env": {
+        "jest": true,
+        "node": true
+    },
     "plugins": [
         "react",
         "jsx-a11y",
