bdf860fd1088f81f2ecc7c5522979ee7cf31dca0
w01fgang <sumin@unix-center.ru>
2017-01-29 13:45:15 +0100

    add main file

--- src/CleanTalkClient.js
+++ src/CleanTalkClient.js
@@ -1,25 +1,73 @@
-import request from 'superagent';
+import request from 'request-promise-native';
 
 class CleanTalkClient {
   constructor({ apiKey }) {
     this.apiKey = apiKey;
+    this.queue = [];
   }
 
-  backlinks({ email = '', nickname = '', ip = '127.0.0.1', jsOn = 1, submitTime = 2 } = {}) {
-    const data = {
+  blacklisted({
+    email = 'stop_email@example.com',
+    nickname = 'John Doe',
+    ip = '127.0.0.1',
+    jsOn = 1,
+    submitTime = 12,
+  } = {}) {
+    this.queue.push({
+      method_name: 'check_message',
       auth_key: this.apiKey,
       sender_email: email,
       sender_nickname: nickname,
       sender_ip: ip,
       js_on: jsOn,
       submit_time: submitTime,
+    });
+    return this;
+  }
+
+  spam({ ip = '', email = '', date = '' } = {}) {
+    this.queue.push({
+      method_name: 'spam_check',
+      auth_key: this.apiKey,
+      ip,
+      email,
+      date,
+    });
+    return this;
+  }
+
+  checkOne(callback) {
+    if (this.queue.length === 0) return callback(new Error('Query queue is empty'));
+    const url = this.queue[0].method_name === 'check_message' ? 'http://moderate.cleantalk.org/api2.0' : 'https://api.cleantalk.org/';
+    const reqOptions = {
+      method: 'POST',
+      url,
+      body: JSON.stringify(this.queue[0]),
     };
-    request
-      .post('/api/pet')
-      .send(data)
-      .end((err, res) => {
-        // Calling the end function will send the request
-      });
+    return request(reqOptions)
+      .then(res => callback(res));
+  }
+
+  checkAll() {
+    return this.data.map(data =>
+      new Promise((resolve, reject) => {
+        request
+          .post('https://moderate.cleantalk.org/api2.0')
+          .send(data)
+          .set('Accept', 'application/json')
+          .end((err, res) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(res);
+            }
+          });
+      }),
+    );
+  }
+
+  reset() {
+    this.queue = [];
   }
 }
 
